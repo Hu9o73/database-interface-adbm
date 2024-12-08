29cc11c088799feadd6ce8d9158cfd21fea790be
Hu9o73 <hbonnell@live.fr>
2024-12-08 01:33:59 +0100

    Not yet implemented page implemented.

--- Frontend/src/app/Config/app.routes.ts
+++ Frontend/src/app/Config/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 
 // Components imports
 
+// Basic and utility components
+import { HomeComponent } from '../components/home/home.component';
+import { NotYetImplementedComponent } from '../components/not-yet-implemented/not-yet-implemented.component';
+
 // Queries
 import { QueryAComponent } from '../components/query-a/query-a.component';
 
 export const routes: Routes = [
-    {path:'queryA', component: QueryAComponent}
+    {path: '', component: HomeComponent},
+    {path:'queryA', component: QueryAComponent},
+    {path:'nye', component: NotYetImplementedComponent}
 ];
---
+++ Frontend/src/app/components/not-yet-implemented/not-yet-implemented.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-not-yet-implemented',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './not-yet-implemented.component.html',
+  styleUrl: './not-yet-implemented.component.css'
+})
+export class NotYetImplementedComponent {
+  
+  constructor(){}
+
+}
