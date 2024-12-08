0d8528da0e9c25b02bd0b28b60d73ead58b3b98e
Hu9o73 <hbonnell@live.fr>
2024-12-08 14:14:49 +0100

    Backend update: Added all the queries endpoints.

--- Backend/src/api/apiAnnotations.ts
+++ Backend/src/api/apiAnnotations.ts
@@ -1,3 +1,4 @@
+// Liveness
 /**
  * @openapi
  * /api/liveness:
@@ -16,7 +17,7 @@
  *               example: "API is alive."
  */
 
-
+// Current container
 /**
  * @openapi
  * /api/database/current-container:
@@ -62,6 +63,7 @@
  */
 
 
+// Query 1
 /**
  * @swagger
  * /api/database/most-popular-per-artist:
@@ -83,49 +85,49 @@
  *                   items:
  *                     type: object
  *                     properties:
- *                       artist_name:
+ *                       ARTIST_NAME:
  *                         type: string
  *                         description: Name of the artist.
- *                       song_name:
+ *                       SONG_NAME:
  *                         type: string
  *                         description: Name of the song.
- *                       popularity:
+ *                       POPULARITY:
  *                         type: integer
  *                         description: Popularity score of the song.
- *                       danceability:
+ *                       DANCEABILITY:
  *                         type: number
  *                         format: float
  *                         description: Danceability score of the song (0-1 scale).
- *                       energy:
+ *                       ENERGY:
  *                         type: number
  *                         format: float
  *                         description: Energy score of the song (0-1 scale).
- *                       valence:
+ *                       VALENCE:
  *                         type: number
  *                         format: float
  *                         description: Valence score of the song (0-1 scale).
- *                       popularity_rank:
+ *                       POPULARITY_RANK:
  *                         type: integer
  *                         description: Rank of the song based on popularity (1-3).
  *                   example:
  *                     [
  *                       {
- *                         "artist_name": "Artist A",
- *                         "song_name": "Song 1",
- *                         "popularity": 95,
- *                         "danceability": 0.8,
- *                         "energy": 0.7,
- *                         "valence": 0.6,
- *                         "popularity_rank": 1
+ *                         "ARTIST_NAME": "Artist A",
+ *                         "SONG_NAME": "Song 1",
+ *                         "POPULARITY": 95,
+ *                         "DANCEABILITY": 0.8,
+ *                         "ENERGY": 0.7,
+ *                         "VALENCE": 0.6,
+ *                         "POPULARITY_RANK": 1
  *                       },
  *                       {
- *                         "artist_name": "Artist B",
- *                         "song_name": "Song 2",
- *                         "popularity": 90,
- *                         "danceability": 0.75,
- *                         "energy": 0.85,
- *                         "valence": 0.55,
- *                         "popularity_rank": 2
+ *                         "ARTIST_NAME": "Artist B",
+ *                         "SONG_NAME": "Song 2",
+ *                         "POPULARITY": 90,
+ *                         "DANCEABILITY": 0.75,
+ *                         "ENERGY": 0.85,
+ *                         "VALENCE": 0.55,
+ *                         "POPULARITY_RANK": 2
  *                       }
  *                     ]
  *       404:
@@ -151,3 +153,381 @@
  *                   description: Error message for internal server errors.
  *                   example: "An error occurred."
  */
+
+
+// Query 2
+/**
+ * @swagger
+ * /api/database/artist-above-average-popularity:
+ *   get:
+ *     summary: Retrieve artists with above-average popularity.
+ *     description: Fetches a list of artists whose average song popularity is higher than the global average song popularity. The response includes details about the artist, their average popularity, and the difference compared to the global average.
+ *     tags:
+ *       - Database
+ *     responses:
+ *       200:
+ *         description: A list of artists with above-average popularity.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 results:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       ARTIST_NAME:
+ *                         type: string
+ *                         description: Name of the artist.
+ *                         example: "FIFTY FIFTY"
+ *                       TOTAL_SONGS:
+ *                         type: integer
+ *                         description: Total number of songs by the artist.
+ *                         example: 1
+ *                       ARTIST_AVG_POPULARITY:
+ *                         type: number
+ *                         format: float
+ *                         description: The average popularity of the artist's songs.
+ *                         example: 97
+ *                       GLOBAL_AVG_POPULARITY:
+ *                         type: number
+ *                         format: float
+ *                         description: The global average song popularity across all songs.
+ *                         example: 38.06
+ *                       POPULARITY_DIFFERENCE:
+ *                         type: number
+ *                         format: float
+ *                         description: The difference between the artist's average popularity and the global average.
+ *                         example: 58.94
+ *       404:
+ *         description: No artists found with above-average popularity.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message.
+ *                   example: "Query didn't yield any result."
+ *       500:
+ *         description: An error occurred while processing the query.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message.
+ *                   example: "An error occurred."
+ */
+
+
+// Query 3
+/**
+ * @swagger
+ * /api/database/song-trend-over-time:
+ *   get:
+ *     summary: Retrieve song trends over time.
+ *     description: Fetches a list of songs with their release date, popularity, and a moving average of popularity over a time window of 30 preceding and 30 following entries. The results are ordered by the release date.
+ *     tags:
+ *       - Songs
+ *     responses:
+ *       200:
+ *         description: A list of songs with their release date, popularity, and moving average popularity.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 results:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       RELEASE_DATE:
+ *                         type: string
+ *                         format: date
+ *                         description: The release date of the song.
+ *                         example: "1956-03-23"
+ *                       SONG_NAME:
+ *                         type: string
+ *                         description: Name of the song.
+ *                         example: "Blue Suede Shoes"
+ *                       POPULARITY:
+ *                         type: integer
+ *                         description: Popularity of the song.
+ *                         example: 68
+ *                       MOVING_AVG_POPULARITY:
+ *                         type: number
+ *                         format: float
+ *                         description: The moving average popularity of the song within the defined time window.
+ *                         example: 40.5806451612903
+ *       404:
+ *         description: No results found for the query.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message.
+ *                   example: "Query didn't yield any result."
+ *       500:
+ *         description: An error occurred while processing the query.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message.
+ *                   example: "An error occurred."
+ */
+
+
+// Query 4
+/**
+ * @swagger
+ * /api/database/song-trend-over-time:
+ *   get:
+ *     summary: Retrieve song trends and statistics for artists with at least 5 songs.
+ *     description: |
+ *       This endpoint retrieves statistical information for artists who have at least 5 songs in the database.
+ *       It includes:
+ *       - Total number of songs.
+ *       - Average popularity (rounded to 2 decimals).
+ *       - Average danceability, energy, and valence (rounded to 3 decimals).
+ *       - Popularity of the most and least popular songs.
+ *     tags:
+ *       - Database
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved artist song trends.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 results:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       ARTIST_NAME:
+ *                         type: string
+ *                         description: Name of the artist.
+ *                         example: "Dua Lipa"
+ *                       TOTAL_SONGS:
+ *                         type: integer
+ *                         description: Total number of songs by the artist.
+ *                         example: 5
+ *                       AVG_POPULARITY:
+ *                         type: number
+ *                         format: float
+ *                         description: Average popularity of the artist's songs (rounded to 2 decimals).
+ *                         example: 74.2
+ *                       AVG_DANCEABILITY:
+ *                         type: number
+ *                         format: float
+ *                         description: Average danceability of the artist's songs (rounded to 3 decimals).
+ *                         example: 0.690
+ *                       AVG_ENERGY:
+ *                         type: number
+ *                         format: float
+ *                         description: Average energy of the artist's songs (rounded to 3 decimals).
+ *                         example: 0.686
+ *                       AVG_VALENCE:
+ *                         type: number
+ *                         format: float
+ *                         description: Average valence of the artist's songs (rounded to 3 decimals).
+ *                         example: 0.496
+ *                       TOP_SONG_POPULARITY:
+ *                         type: integer
+ *                         description: Popularity of the artist's most popular song.
+ *                         example: 84
+ *                       LOWEST_SONG_POPULARITY:
+ *                         type: integer
+ *                         description: Popularity of the artist's least popular song.
+ *                         example: 65
+ *       404:
+ *         description: No results found.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message indicating no results were found.
+ *                   example: "Query didn't yield any result."
+ *       500:
+ *         description: Server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message for a server-side issue.
+ *                   example: "An error occurred."
+ */
+
+
+// Query 5
+/**
+ * @swagger
+ * /api/database/song-rank-within-category:
+ *   get:
+ *     summary: Retrieve song rankings within popularity categories.
+ *     description: |
+ *       This endpoint retrieves songs classified into popularity categories (`High`, `Medium`, `Low`) based on their popularity scores.
+ *       It includes:
+ *       - Song name, artist name, popularity score, energy, and danceability.
+ *       - Ranking of songs within their respective categories, ordered by popularity.
+ *       - Results are filtered for the `Low` popularity category.
+ *     tags:
+ *       - Database
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved song rankings within categories.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 results:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       POPULARITY_CATEGORY:
+ *                         type: string
+ *                         description: The popularity category of the song (`High`, `Medium`, `Low`).
+ *                         example: "Low"
+ *                       SONG_NAME:
+ *                         type: string
+ *                         description: Name of the song.
+ *                         example: "Waiting Game"
+ *                       ARTIST_NAME:
+ *                         type: string
+ *                         description: Name of the artist.
+ *                         example: "Parson James"
+ *                       POPULARITY:
+ *                         type: integer
+ *                         description: Popularity score of the song.
+ *                         example: 49
+ *                       ENERGY:
+ *                         type: integer
+ *                         description: Energy level of the song.
+ *                         example: 307
+ *                       DANCEABILITY:
+ *                         type: integer
+ *                         description: Danceability level of the song.
+ *                         example: 393
+ *                       CATEGORY_RANK:
+ *                         type: integer
+ *                         description: Rank of the song within its popularity category.
+ *                         example: 1
+ *       404:
+ *         description: No results found.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message indicating no results were found.
+ *                   example: "Query didn't yield any result."
+ *       500:
+ *         description: Server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message for a server-side issue.
+ *                   example: "An error occurred."
+ */
+
+
+// Query 6
+/**
+ * @swagger
+ * /api/database/song-rank-within-category:
+ *   get:
+ *     summary: Retrieve highly popular and energetic songs.
+ *     description: |
+ *       This endpoint retrieves songs with an energy level above 600 and a popularity score above 75.
+ *       The results are sorted by popularity in descending order and include song name, artist name, energy, danceability, and popularity.
+ *     tags:
+ *       - Songs
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved popular and energetic songs.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 results:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       SONG_NAME:
+ *                         type: string
+ *                         description: The name of the song.
+ *                         example: "Flowers"
+ *                       ARTIST_NAME:
+ *                         type: string
+ *                         description: The name of the artist.
+ *                         example: "Miley Cyrus"
+ *                       ENERGY:
+ *                         type: integer
+ *                         description: The energy level of the song.
+ *                         example: 681
+ *                       DANCEABILITY:
+ *                         type: integer
+ *                         description: The danceability level of the song.
+ *                         example: 707
+ *                       POPULARITY:
+ *                         type: integer
+ *                         description: The popularity score of the song.
+ *                         example: 96
+ *       404:
+ *         description: No results found.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message indicating no results were found.
+ *                   example: "Query didn't yield any result."
+ *       500:
+ *         description: Server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message for a server-side issue.
+ *                   example: "An error occurred."
+ */
+
+
+
+
+
--- Backend/src/api/dbQueriesEndpoints.ts
+++ Backend/src/api/dbQueriesEndpoints.ts
@@ -3,6 +3,7 @@ import sequelize from '../ConfigFiles/dbConfig';
 
 const router = Router()
 
+// Query 1
 router.get('/api/database/most-popular-per-artist', (req: Request, res: Response) => {
     sequelize
         .query(
@@ -37,5 +38,185 @@ router.get('/api/database/most-popular-per-artist', (req: Request, res: Response
             res.status(500).json({ error: 'An error occurred.' });
           });
 });
+
+// Query 2
+router.get('/api/database/artist-above-average-popularity', (req: Request, res: Response) => {
+    sequelize
+        .query(
+            `
+            SELECT 
+                art.artist_name,
+                COUNT(s.song_id) as total_songs,
+                ROUND(AVG(s.popularity), 2) as artist_avg_popularity,
+                ROUND((SELECT AVG(popularity) FROM songs), 2) as global_avg_popularity,
+                ROUND(AVG(s.popularity) - (SELECT AVG(popularity) FROM songs), 2) as popularity_difference
+            FROM artists art
+            JOIN albums alb ON art.artist_id = alb.artist_id
+            JOIN songs s ON alb.album_id = s.album_id
+            GROUP BY art.artist_name
+            HAVING AVG(s.popularity) > (
+                SELECT AVG(popularity) FROM songs
+            )
+            ORDER BY artist_avg_popularity DESC;
+            `
+        )
+        .then(([results]: any) => {
+            if (results && results.length > 0) {
+                res.status(200).json({ results });
+            } else {
+                res.status(404).json({error: "Query didn't yield any result." });
+            }
+          })
+          .catch((error) => {
+            console.error('Error executing query:', error);
+            res.status(500).json({ error: 'An error occurred.' });
+          });
+});
+
+// Query 3
+router.get('/api/database/song-trend-over-time', (req: Request, res: Response) => {
+    sequelize
+        .query(
+            `
+            SELECT 
+                alb.release_date,
+                s.song_name,
+                s.popularity,
+                ROUND(AVG(s.popularity) OVER (
+                    ORDER BY alb.release_date 
+                    ROWS BETWEEN 30 PRECEDING AND 30 FOLLOWING
+                ),2) as moving_avg_popularity
+            FROM songs s
+            JOIN albums alb ON s.album_id = alb.album_id
+            ORDER BY alb.release_date;
+            `
+        )
+        .then(([results]: any) => {
+            if (results && results.length > 0) {
+                res.status(200).json({ results });
+            } else {
+                res.status(404).json({error: "Query didn't yield any result." });
+            }
+          })
+          .catch((error) => {
+            console.error('Error executing query:', error);
+            res.status(500).json({ error: 'An error occurred.' });
+          });
+});
+
+// Query 4
+router.get('/api/database/top-artists', (req: Request, res: Response) => {
+    sequelize
+        .query(
+            `
+            SELECT 
+                art.artist_name,
+                COUNT(DISTINCT s.song_id) as total_songs,
+                ROUND(AVG(s.popularity), 2) as avg_popularity,
+                ROUND(AVG(af.danceability), 3) as avg_danceability,
+                ROUND(AVG(af.energy), 3) as avg_energy,
+                ROUND(AVG(af.valence), 3) as avg_valence,
+                MAX(s.popularity) as top_song_popularity,
+                MIN(s.popularity) as lowest_song_popularity
+            FROM artists art
+            JOIN albums alb ON art.artist_id = alb.artist_id
+            JOIN songs s ON alb.album_id = s.album_id
+            JOIN audio_features af ON s.song_id = af.song_id
+            GROUP BY art.artist_name
+            HAVING COUNT(DISTINCT s.song_id) >= 5
+            ORDER BY avg_popularity DESC;
+            `
+        )
+        .then(([results]: any) => {
+            if (results && results.length > 0) {
+                res.status(200).json({ results });
+            } else {
+                res.status(404).json({error: "Query didn't yield any result." });
+            }
+          })
+          .catch((error) => {
+            console.error('Error executing query:', error);
+            res.status(500).json({ error: 'An error occurred.' });
+          });
+});
+
+// Query 5
+router.get('/api/database/song-rank-within-category', (req: Request, res: Response) => {
+    sequelize
+        .query(
+            `
+            WITH popularity_categories AS (
+            SELECT song_id, song_name,
+                CASE 
+                    WHEN popularity >= 80 THEN 'High'
+                    WHEN popularity >= 50 THEN 'Medium'
+                    ELSE 'Low'
+                END as popularity_category
+            FROM songs
+            )
+            SELECT 
+                pc.popularity_category,
+                s.song_name,
+                art.artist_name,
+                s.popularity,
+                af.energy,
+                af.danceability,
+                RANK() OVER (PARTITION BY pc.popularity_category ORDER BY s.popularity DESC) as category_rank
+            FROM popularity_categories pc
+            JOIN songs s ON pc.song_id = s.song_id
+            JOIN albums alb ON s.album_id = alb.album_id
+            JOIN artists art ON alb.artist_id = art.artist_id
+            JOIN audio_features af ON s.song_id = af.song_id
+            WHERE pc.popularity_category = 'Low'
+            ORDER BY pc.popularity_category, category_rank;
+            `
+        )
+        .then(([results]: any) => {
+            if (results && results.length > 0) {
+                res.status(200).json({ results });
+            } else {
+                res.status(404).json({error: "Query didn't yield any result." });
+            }
+          })
+          .catch((error) => {
+            console.error('Error executing query:', error);
+            res.status(500).json({ error: 'An error occurred.' });
+          });
+});
+
+// Query 6
+router.get('/api/database/popular-high-energy', (req: Request, res: Response) => {
+    sequelize
+        .query(
+            `
+            SELECT 
+                s.song_name,
+                art.artist_name,
+                af.energy,
+                af.danceability,
+                s.popularity
+            FROM songs s
+            JOIN albums alb ON s.album_id = alb.album_id
+            JOIN artists art ON alb.artist_id = art.artist_id
+            JOIN audio_features af ON s.song_id = af.song_id
+            WHERE af.energy > 600 
+            AND s.popularity > 75
+            ORDER BY s.popularity DESC;
+
+            `
+        )
+        .then(([results]: any) => {
+            if (results && results.length > 0) {
+                res.status(200).json({ results });
+            } else {
+                res.status(404).json({error: "Query didn't yield any result." });
+            }
+          })
+          .catch((error) => {
+            console.error('Error executing query:', error);
+            res.status(500).json({ error: 'An error occurred.' });
+          });
+});
+
 
 export default router;
